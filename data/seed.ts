74b3a43b52115a47fa9856ab056f03652649b63d
Your Name <your.email@example.com>
2025-03-25 19:51:02 -0400

    adding more inline comments

--- data/seed.ts
+++ data/seed.ts
@@ -5,11 +5,14 @@ import csvParser from "csv-parser";
 import stripBom from "strip-bom-stream";
 import bcrypt from "bcryptjs";
 
+// Define directory where the database and seed data are stored
 const dataDir = path.join(process.cwd(), "data");
+// Ensure that the data directory exists, create if necessary
 if (!fs.existsSync(dataDir)) {
   fs.mkdirSync(dataDir, { recursive: true });
 }
 
+// Define database filepath
 const dbPath = path.join(dataDir, "marz.db");
 
 // Delete existing database file to reset everything
@@ -17,21 +20,23 @@ if (fs.existsSync(dbPath)) {
   fs.unlinkSync(dbPath);
 }
 
+// Init a new SQLite3 Database instance
 const db = Database(dbPath);
 
-// Read CSV
+/* Read CSV */
 async function readCSV(filePath: string) {
   return new Promise((resolve, reject) => {
     const results = [];
     fs.createReadStream(filePath)
-      .pipe(stripBom())
-      .pipe(csvParser())
-      .on("data", (data) => results.push(data))
-      .on("end", () => resolve(results))
-      .on("error", (error) => reject(error));
+      .pipe(stripBom()) // remove byte order mark if it exists
+      .pipe(csvParser()) // parse the csv
+      .on("data", (data) => results.push(data)) // collect parsed rows
+      .on("end", () => resolve(results)) // resolve w/ collected data
+      .on("error", (error) => reject(error)); // reject on error
   });
 }
 
+/* Crates necessary db tables if they don't exist yet */
 async function createTables() {
   try {
     db.exec(`
@@ -45,26 +50,29 @@ async function createTables() {
   }
 }
 
+/* Seeds database w/ initial usr data from csv file */
 async function seedDatabase() {
   try {
     console.log("Seeding database...");
-
+    // Read usr data from Users.csv
     const users = await readCSV(path.join(dataDir, "seeding", "Users.csv"));
-
+    // Generate salt for hasing pws
     const salt = await bcrypt.genSalt(10);
     console.log("Salt (for all users):", salt);
-
+    // Inserting usrs
     console.log("Inserting Users...");
     const insertUser = db.prepare(
       "INSERT INTO users (email, password) VALUES (?, ?)",
     );
-
+    // Using transaction to insert multiple users at one go
     const insertMany = db.transaction((users) => {
       for (const user of users) {
+        // Hashing usr pw using generated salt and inserting it into the db
         insertUser.run(user.email, bcrypt.hashSync(user.password, salt));
       }
     });
 
+    // Executing transcation to insert all usrs
     insertMany(users);
     console.log("Database successfully seeded.");
   } catch (error) {
