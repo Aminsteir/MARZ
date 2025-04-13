fcb7a5e2030a271988e7df6e25919e88b6c8efd1
Your Name <your.email@example.com>
2025-04-13 17:39:45 -0400

    importing necessary libraries

--- src/app/api/seller-profile/route.tsx
+++ src/app/api/seller-profile/route.tsx
@@ -1,1 +1,6 @@
-import { registerUser } from "@/services/userService";
+import { NextRequest, NextResponse } from "next/server";
+import db from "@/db/db";
+import bcrypt from "bcryptjs";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route"
+import { v4 as uuidv4 } from "uuid";
