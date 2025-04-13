07ca6ef6a59f9a15a947fccd2d03f57cff4f792e
Your Name <your.email@example.com>
2025-04-13 18:20:01 -0400

    starting PUT method, need to finish verifying input and updating db

--- src/app/api/buyer-profile/route.tsx
+++ src/app/api/buyer-profile/route.tsx
@@ -5,11 +5,11 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route"
 import { v4 as uuidv4 } from "uuid";
 
-/** GET method: reutnrs the buyer's profile info. */
+/** GET method: reutnrs the buyer's profile info. **/
 export async function GET(req: NextRequest) {
     // getting current session and checking that it's valid
     const session = await getServerSession(authOptions);
-    if (!session.user?.email) {
+    if (!session?.user?.email) {
         return NextResponse.json({error: "unauthorized"}, {status:401});
     } 
 
@@ -39,4 +39,48 @@ export async function GET(req: NextRequest) {
     }
 
     return NextResponse.json(profile, {status: 200});
+}
+
+/** PUT method: update the Buyer's profile info. w/ the inputted data **/
+export async function PUT(req:NextRequest) {
+    // getting current session and checking that it's valid
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.email) {
+        return NextResponse.json({error: "Unauthorized User"}, {status:401});
+    } 
+
+    // getting email of buyers
+    const email = session.user.email;
+    const body = await req.json();
+    // getting the input
+    const {
+        business_name,
+        street_number,
+        street_name,
+        city,
+        state,
+        zipcode,
+        password
+    } = body;
+
+    // Checking Inputs 
+    // checking number of fields
+    if (!business_name || !street_number || !street_name || !city || !state || !zipcode) {
+        return NextResponse.json({error: "Missing fileds"}, {status: 400});
+    }
+
+    // get and check Buyer email
+    const buyer = db.prepare("SELECT * FROM Buyer WHERE email = ?").get(email);
+    if(!buyer) {
+        return NextResponse.json({error: "Buyer was not found"}, {status:404});
+    }
+
+    // get and check Buyer address
+    const address = db
+                    .prepare("SELECT buyer_address_id FROM Buyer WHERE email = ?")
+                    .get(email) as { buyer_address_id: string } | undefined;
+    const address_id = address?.buyer_address_id;
+    if (!address_id) {
+        return NextResponse.json({error: "Address was not found"}, {status: 404});
+    }
 }
