8ba8e4668e0a8d990422641691b906dfeef962ce
Your Name <your.email@example.com>
2025-04-22 13:02:29 -0400

    add comments for clarity

--- src/app/api/add-to-cart/route.ts
+++ src/app/api/add-to-cart/route.ts
@@ -4,8 +4,10 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { UserRole } from "@/db/models";
 
+// Method POST: if successful, add a Product to a Buyer's cart; else, show an error message
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
+  // check if the user is a Buyer (only Buyer's can add to a shopping cart)
   if (
     !session ||
     !session.user ||
@@ -14,26 +16,31 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
+  // get Buyer's email
   const buyer_email = session.user.email;
   const body = await req.json();
 
+  // get Seller's email and product listing
   const seller_email = body.seller_email;
   const listing_id = body.listing_id;
   const quantity = 1;
 
+  // adding order information to the cart
   const result = await addProductToCart(
     buyer_email,
     seller_email,
     listing_id,
     quantity,
   )
-    .then(() => {
-      return { msg: "Cart updated successfully", status: true };
+    .then(() => { // successful message 
+      return { msg: "Cart updated successfully", status: true }; 
     })
-    .catch((err) => {
+    .catch((err) => { // error message
       console.error("Error adding product to cart:", err);
       return { msg: err.message, status: false };
     });
+
+  // return status of whether the product was added to the cart or not
   if (result.status) {
     return NextResponse.json({ message: result.msg }, { status: 201 });
   }
--- src/app/api/confirm-checkout/route.ts
+++ src/app/api/confirm-checkout/route.ts
@@ -3,15 +3,19 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { confirmCheckout } from "@/services/checkoutService";
 
+// Method POST: if successful, allow checkout page the checkout button is clicked from the shopping cart; else, show an error message
 export async function POST(req: Request) {
     const session = await getServerSession(authOptions);
 
+    // check if the user exists (should be a Buyer)
     if (!session || !session.user || !session.user.email) {
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
+    // get Buyer email
     const buyerEmail = session.user.email;
 
+    // retrieve status of the confirmCheckout() method and return message accordingly
     try {
         const orderIds = await confirmCheckout(buyerEmail);
         return NextResponse.json({ message: "Checkout successful", orderIds });
