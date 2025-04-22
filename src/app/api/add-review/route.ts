3e2708aa692e9e4043e4c0ebcfbccd7153e89c41
Your Name <your.email@example.com>
2025-04-22 12:42:31 -0400

    add comments for clarity

--- src/app/api/add-review/route.ts
+++ src/app/api/add-review/route.ts
@@ -4,8 +4,10 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { Review, UserRole } from "@/db/models";
 
+// Method POST: if successful, add a review from a Buyer; else, show an error message
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
+  // check if the user is a Buyer
   if (
     !session ||
     !session.user ||
@@ -14,17 +16,21 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 
+  // get Buyer email
   const buyer_email = session.user.email;
   const body = await req.json();
 
+  // get the inputted review submission
   const review: Review = {
     order_id: body.order_id,
     rating: body.rating,
     review_desc: body.review_desc,
   };
 
+  // success message
   let message = "Review added successfully";
 
+  // attempt to add review
   const status = await addReview(buyer_email, review)
     .then(() => true)
     .catch((err) => {
@@ -33,6 +39,7 @@ export async function POST(req: Request) {
       return false;
     });
 
+  // return status of adding the review via the message (success or failure) assigned above
   if (status) {
     return NextResponse.json({ message }, { status: 201 });
   }
