f379ab538c9fb8a4cd03542cff7c0017095b528f
Your Name <your.email@example.com>
2025-04-22 12:26:04 -0400

    adding comments for clarification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+/** This is a CSS Styling class for custom layout design for our homepage */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import AuthProvider from "@/context/AuthProvider";
 
+// Init Gueist sans font
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -14,11 +16,13 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// Def metadata for application (used for browser tab info)
 export const metadata: Metadata = {
   title: "NittanyBusiness",
   description: "NittanyBusiness marketplace platform",
 };
 
+// Def RootLayout component, which wraps the entire app layout
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
