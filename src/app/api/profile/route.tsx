5ce277e2e43cc04a0be95d70f0a1cebd53a3d2f2
Amin Armat <amin@ibm.com>
2025-04-28 14:53:39 -0400

    seller update profile

--- src/app/api/profile/route.tsx
+++ src/app/api/profile/route.tsx
@@ -54,7 +54,7 @@ export async function GET(req: NextRequest) {
             s.balance
         FROM Users u
         JOIN Sellers s ON u.email = s.email
-        JOIN Address a ON s.seller_address_id = a.address_id
+        JOIN Address a ON s.business_address_id = a.address_id
         JOIN Zipcode_Info z ON a.zipcode = z.zipcode
         WHERE u.email = ?`,
       )
